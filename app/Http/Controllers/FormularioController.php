aa9ec7165f410b25879ca177da4b9752387cc930
paul <pcruz.191ci13@istta.edu.pe>
2021-11-17 00:22:20 -0500

    dd

---
+++ app/Http/Controllers/FormularioController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Formulario;
+
+class FormularioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $reglas = [
+        'nombre' => 'required',
+        'apellido' => 'required',
+        'correo' => 'required|email',
+        'numero' => 'required'
+        ];
+        $mensajeDeError = [
+        'required' => 'Ingreso de datos erróneo'
+        ];
+        $this->validate($request, $reglas, $mensajeDeError);
+        $formulario= new Formulario();
+        
+        $formulario->nombre = $request->get('nombre');
+        $formulario->apellido = $request->get('apellido');
+        $formulario->correo = $request->get('correo');
+        $formulario->numero = $request->get('numero');
+        
+        $formulario->save();
+
+        return redirect('/');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2021_11_13_154202_create_formularios_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFormulariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('formularios', function (Blueprint $table) {
+            $table->id();
+            $table->String("nombre",20);
+            $table->String("apellido",20);
+            $table->String("correo",50);
+            $table->String("numero",9);            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('formularios');
+    }
+}
