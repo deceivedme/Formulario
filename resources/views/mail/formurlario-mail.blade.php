b040c7f3387e2fb4bfd688fd679334e120733995
williams <warriaga.191ci03@istta.edu.pe>
2021-11-18 12:24:06 -0500

    Arreglando text

--- resources/views/mail/formurlario-mail.blade.php
+++ resources/views/mail/formurlario-mail.blade.php
@@ -1,8 +1,11 @@
 <body>
-    <h1>THE LAST WEEK OF INFORMARICS CI</h1>
+    <h1>THE LAST WEEK OF INFORMATICS CI</h1>
+
     <p>Ud se registro correctamente con los siguiente información: </p>
+    <p>----------------------------------------------</p>
     <p><strong>Nombre:</strong> {{$informacion['nombre']}}</p>
     <p><strong>Apellido:</strong> {{$informacion['apellido']}}</p>
     <p><strong>Correo:</strong> {{$informacion['correo']}}</p>
     <p><strong>Numero:</strong> {{$informacion['numero']}}</p>
+    <p>----------------------------------------------</p>
 </body>
