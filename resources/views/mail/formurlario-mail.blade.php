6ce2d890a4498943258447498052a7c0688b659a
LuisMiguel <ochoaarizaluis@gmail.com>
2021-11-18 12:02:39 -0500

    cambio correo

--- resources/views/mail/formurlario-mail.blade.php
+++ resources/views/mail/formurlario-mail.blade.php
@@ -2,7 +2,7 @@
     <h1>Correo elctrónico</h1>
     <p>Este es el primer correo que mandaré por Laravel</p>
     <p><strong>Nombre:</strong> {{$informacion['nombre']}}</p>
-    <p><strong>Nombre:</strong> {{$informacion['apellido']}}</p>
-    <p><strong>Nombre:</strong> {{$informacion['correo']}}</p>
-    <p><strong>Nombre:</strong> {{$informacion['numero']}}</p>
+    <p><strong>Apellido:</strong> {{$informacion['apellido']}}</p>
+    <p><strong>Correo:</strong> {{$informacion['correo']}}</p>
+    <p><strong>Numero:</strong> {{$informacion['numero']}}</p>
 </body>
